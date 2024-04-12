41f3642af6bfe31924a93ad985acb310a234c5a0
Xinlong Wu <821408745@qq.com>
2024-04-13 00:04:14 +1000

    add env file and swagger API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+
+if (!process.env.NODE_ENV) {
+  Logger.error('NODE_ENV is not defined, it should be set to development or production', 'Environment');
+  process.exit(1);
+}
+
+let envFilePath = '.env/.'+process.env.NODE_ENV+'.env';
+Logger.log('Loading environment variables from: ' + envFilePath, 'Environment');
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+
+  if (process.env.NODE_ENV === 'development') {
+      const config = new DocumentBuilder()
+      .setTitle('Moral Decisions API')
+      .setDescription('The Moral Decisions API description')
+      .setVersion('0.0.1')
+      .addTag('default')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
+  await app.listen(5000, () => {
+    // console.log(`Server running on http://localhost:${process.env.SERVER_PORT}`);
+    Logger.log('Server running on http://localhost:' + process.env.SERVER_PORT, 'Bootstrap');
+  });
 }
 bootstrap();
