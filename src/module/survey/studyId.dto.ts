0416f5aa8dd45259fbf1d91acd7adb2368141429
Xinlong Wu <821408745@qq.com>
2024-04-17 22:13:22 +1000

    Fix linting issues and add new survey question endpoint

---
+++ src/module/survey/studyId.dto.ts
@@ -0,0 +1,14 @@
+// more usage, refer to https://github.com/typestack/class-validator?tab=readme-ov-file#usage
+import { IsNotEmpty} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class StudyIdDto {
+
+    @ApiProperty({
+        description: 'The study id',
+        required: true,
+        // enum: [1, 2, 3, 4]
+    })
+    @IsNotEmpty()
+    surveyType: string;
+}
