8d7b1e098143f6e785d91c3d9e899a63eadef251
Xinlong Wu <821408745@qq.com>
2024-04-17 22:13:22 +1000

    Fix linting issues and add new survey question endpoint

--- src/controller/survey/survey.controller.ts
+++ src/controller/survey/survey.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { SurveyTypeDto } from '../../module/survey/surveyType.dto';
+// import { SurveyTypeDto } from '../../module/survey/surveyType.dto';
 
 @Controller('survey')
 export class SurveyController {
 
     @Get('surveyId')
-    getSurveyId(@Query() query: SurveyTypeDto) {
+    getSurveyId(@Query() query) {
         return query;
+    }
+
+    @Get('questions')
+    getQuestion(@Query() param) {
+        return param.surveyType;
     }
 }
---
+++ src/module/survey/question.dto.ts
@@ -0,0 +1,14 @@
+
+export class QuestionDto {
+    /**
+     * {
+     *   id: "survey-id-1",
+     *   title: "Survey Title 1",
+     *   text: "Survey Question 1"
+     * }
+     */
+
+    id: string;
+    title: string;
+    text: string;
+}
