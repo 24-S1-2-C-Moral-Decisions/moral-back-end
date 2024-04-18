3aeab2c9dc87c548478594cb0cae43f0770dd154
c1c4da3300 <fuceming9@gmail.com>
2024-04-18 20:51:16 +1000

    database integration

--- src/controller/survey/survey.controller.ts
+++ src/controller/survey/survey.controller.ts
@@ -2,31 +2,32 @@ import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { StudyIdDto } from '../../module/survey/studyId.dto';
 import { AnswersDto } from '../../module/survey/answers.dto';
-import { QuestionDto } from '../../module/survey/question.dto';
+import { SurveyService } from 'src/service/survey.service';
 
 
 @Controller('survey')
 @ApiTags('survey')
 export class SurveyController {
+    constructor(private surveyService: SurveyService) {}
 
     @Get('questions')
     getQuestion(@Query() studyId: StudyIdDto) {
         /**
          * {
-         *   id: "survey-id-1",
+         *   _id: "survey-id-1",
          *   title: "Survey Title 1",
          *   text: "Survey Question 1"
+         *   count
+         *   studyId
          * }
          */
-        return new QuestionDto({
-            id: studyId.studyId,
-            title: "Survey Title 1",
-            text: "Survey Question 1"
-        });
-    }
 
+        return this.surveyService.findQuestions(studyId);
+    }
+    
     @Post('answers')
     poetAnswers(@Body() body : AnswersDto) {
-        return body.studyId;
+        this.surveyService.createAnswers(body);
+        return "answer saved";
     }
 }
--- src/module/survey/question.dto.ts
+++ src/module/survey/question.dto.ts
@@ -4,8 +4,12 @@ export class QuestionDto {
         this.id = question.id;
         this.title = question.title;
         this.text = question.text;
+        this.count=question.count;
+        this.studyId=question.studyId;
     }
     id: string;
     title: string;
     text: string;
+    count: number;
+    studyId: string;
 }
