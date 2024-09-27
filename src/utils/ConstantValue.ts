eb792c5cdfd83891412f8e4ba65fb4b5877b3e5e
Xinlong Wu <821408745@qq.com>
2024-09-28 00:51:22 +1000

    Add constant values for database names and connection names

---
+++ src/utils/ConstantValue.ts
@@ -0,0 +1,16 @@
+import exp from "constants";
+
+export const DatabaseType = 'mongodb';
+export const PostsDBName = 'posts';
+export const SurveyDBName = 'survey';
+export const CacheDBName = 'cache';
+export const RedditDBName = 'reddit';
+
+export const PostConnectionName = 'posts';
+export const SurveyConnectionName = 'survey';
+export const CacheConnectionName = 'cache';
+
+export const AllCollectionName = 'all';
+export const PostSummaryCollectionName = 'post_summary';
+export const SubmissionCollectionName = 'submissions';
+
